90ab748b11789f889f2b43efa4aa14222cc159d7
Chiharu Shinohara <chiharu.shinohara@nadia-corp.co.jp>
2021-05-28 14:48:14 +0900

    add FetchWeatherInfo

--- 2_asyncFn/__tests__/FetchWeatherInfo.test.js
+++ 2_asyncFn/__tests__/FetchWeatherInfo.test.js
@@ -1,3 +1,12 @@
 const FetchWeatherInfo = require("../modules/FetchWeatherInfo");
 
-describe("FetchWeatherInfo Test", () => {});
+describe("FetchWeatherInfo Test", () => {
+  test('get data of PublishOffice',()=>{
+     expect.assertions(1); 
+      
+   return FetchWeatherInfo().then(value=>{
+        expect(value).toBe("気象庁");
+      })
+     
+  })
+});
